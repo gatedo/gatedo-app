e94e87478728d211d20fb0432dadabdc4ece45ad
diegobtv <planoastartup@gmail.com>
2026-04-08 20:30:22 -0300

    fix: correct gamification module relative imports

--- backend/src/gamification/gamification.module.ts
+++ backend/src/gamification/gamification.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { GamificationController } from '../gamification/gamification.controller';
-import { GamificationService } from '../gamification/gamification.service';
+import { GamificationController } from './gamification.controller';
+import { GamificationService } from './gamification.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationModule } from '../notifications/notification.module';
 
